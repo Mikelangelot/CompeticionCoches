c5f7ee9a1592a85c5bb45cf23b98c2d7e88852ec
unknown <isabellanottaro@gmail.com>
2026-02-19 13:18:55 +0100

    Metodos y constructor de circuito implementados

--- src/Circuito.java
+++ src/Circuito.java
@@ -4,6 +4,24 @@ public class Circuito {
 
     private String nombreCircuito;
     private int longitudCircuitoKM;
-    private int [] arrayCarreras;
+    private Carrera [] arrayCarreras;
 
+
+    // Constructor de Circuito, llamando a los atributos privados
+
+    public Circuito(String nombreCircuito, int longitudCircuitoKM, Carrera[] arrayCarreras) {
+        this.nombreCircuito = nombreCircuito;
+        this.longitudCircuitoKM = longitudCircuitoKM;
+        this.arrayCarreras = arrayCarreras;
+    }
+
+
+    //Métodos
+    public void anhadirCarrera(carrera:Carrera){
+
+    }
+
+    public int obtenerCarrerasTotales(){
+
+    }
 }
