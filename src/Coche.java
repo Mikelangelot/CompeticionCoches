ac24b658ab29742f770bba6763f977a4e4f1c0bb
carlos sanchez criado <carlosanchez0897@gmail.com>
2026-02-19 12:58:18 +0100

    primeros pasos de clase coche

--- src/Coche.java
+++ src/Coche.java
@@ -1,2 +1,17 @@
+import java.util.Random;
 public class Coche {
-}
+
+    private String escuderia;
+    private double velocidadMaxima;
+    private double fiabilidad;
+
+    public Coche (String escuderia, double velocidadMaxima, double fiabilidad){
+        this.escuderia=escuderia;
+        this.velocidadMaxima=velocidadMaxima;
+        this.fiabilidad=fiabilidad;
+
+    }
+
+
+    }
+
