560ef3144003a5f49ba5e648bf53971b8b8bfe3f
agussvn <neivagus@gmail.com>
2026-02-19 13:18:58 +0100

    Atributos creados(cambiar variable en el futuro)

--- src/Competicion.java
+++ src/Competicion.java
@@ -1,2 +1,18 @@
 public class Competicion {
+
+    //Atributos con arrays de piloto y carrera.
+    private String nombreComp;
+    private Piloto[] pilotos;
+    private Carrera[] carreras;
+    private int numPilotos;
+    private int numCarreras;
+    private boolean campeonatoIniciado;
+
+
+
+    public String getNombreComp(){
+        return nombreComp;
+    }
+
+
 }
