9724457440c91e91baf88d94a5f8dccde7e419e4
agussvn <neivagus@gmail.com>
2026-02-19 13:41:35 +0100

    Métodos comp hechos falta ajustar variables

--- src/Competicion.java
+++ src/Competicion.java
@@ -8,11 +8,33 @@ public class Competicion {
     private int numCarreras;
     private boolean campeonatoIniciado;
 
-
+    //Respectivo Constructor
+    public Competicion(String nombreComp,int maxPilotos,int maxCarreras){
+        this.nombreComp=nombreComp;
+        this.pilotos=new Piloto[maxPilotos];
+        this.carreras=new Carrera[maxCarreras];
+        this.numPilotos=0;
+        this.numCarreras=0;
+    }
 
     public String getNombreComp(){
         return nombreComp;
     }
+    //Métodos
+     public boolean anhadirPiloto(Piloto piloto){
+        return false;
+     }
+     public boolean anhadirCarrera(Circuito circuito, int vueltas){
+        return false;
+     }
+     public boolean arrancarCampeonato(){
+        return false;
+    }
+     public void imprimirResultado(){
+     }
+    public void imprimirResultadoEscuderia() {
+    }
 
 
 }
+
