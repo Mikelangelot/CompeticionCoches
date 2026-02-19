ebe10a1e2882f0bd6d21c029caddaa5c749a7918
carlos sanchez criado <carlosanchez0897@gmail.com>
2026-02-19 13:36:08 +0100

    intento de esqueleto de Piloto

--- src/Piloto.java
+++ src/Piloto.java
@@ -3,4 +3,17 @@ public class Piloto {
     private int edad;
     private Coche coche;
     private double habilidad;
+
+    public Piloto(String nombre, int edad,Coche coche, double habilidad){
+    }
+
+    public double velocidadMedia(){
+        return 0;
+    }
+
+    public String getNombre(){return null;}
+    public int getEdad() {return 0;}
+    public Coche getCoche(){return null;}
+    public String mostrarPiloto(){return null;}
+
 }
