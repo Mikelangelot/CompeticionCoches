6bf4454c1e46029e7ea1d60ca5172c37bcb1531c
Mikelangelot <miangelcabo@gmail.com>
2026-02-20 12:56:14 +0100

    Preguntar si la fiabilidad puede ser boolean ya que son dos parametros y si el método acabar carrera hay que utilizarlo con un random

--- src/Coche.java
+++ src/Coche.java
@@ -3,20 +3,27 @@ public class Coche {
 
     private String escuderia;
     private double velocidadMaxima;
-    private double fiabilidad;
+    private boolean fiabilidad;
 
-    public Coche (String escuderia, double velocidadMaxima, double fiabilidad){
+    public Coche (String escuderia, double velocidadMaxima, boolean fiabilidad){
         this.escuderia=escuderia;
         this.velocidadMaxima=velocidadMaxima;
         this.fiabilidad=fiabilidad;
 
     }
     public boolean acabarCarrera (int kilometros){
-        return false;
-
-    }public String getEscuderia(){return null;}
-    public double getVelocidadMaxima(){return 0;}
-    public String mostrarCoche(){return null;}
+        boolean fiabilidad;
+        return true;
 
     }
+    public String getEscuderia(){
+        return null;
+    }
+    public double getVelocidadMaxima(){
+        return 0;
+    }
+    public String mostrarCoche(){
+        return null;
+    }
+}
 
--- src/Simulador.java
+++ src/Simulador.java
@@ -1,5 +1,5 @@
 public class Simulador {
     public static void main(String[] args) {
-        
+        Coche coche1 = new Coche("alfa", 22, false);
     }
 }
