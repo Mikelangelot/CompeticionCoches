cbb761e5a948a09828be58250730b1f8448b8281
Mikelangelot <miangelcabo@gmail.com>
2026-02-24 10:07:27 +0100

    acabada clase piloto conviene revisar el método velocidad que no me fío mucho

--- src/Piloto.java
+++ src/Piloto.java
@@ -8,15 +8,22 @@ public class Piloto {
     private Random rand;
 
     public Piloto(String nombre, int edad,Coche coche, String inputHabilidad){
-        if (inputHabilidad.equalsIgnoreCase( "Excelente"){
-            habilidad
+        if (inputHabilidad.equalsIgnoreCase( "Excelente")){
+            //hacer un random entre 0,9 y 1
+            habilidad = 0.9 + (rand.nextDouble() * 0.1);
+        } else if (inputHabilidad.equalsIgnoreCase("Bueno")){
+            habilidad = 0.8 + (rand.nextDouble() * 0.1);
+        } else if (inputHabilidad.equalsIgnoreCase("Mediocre")){
+            habilidad = 0.7 + (rand.nextDouble() * 0.1);
+        } else{
+            habilidad = 0.6;
         }
     }
 
     public double velocidadMedia(){
         double factorAleatorio = 0.9 + (rand.nextDouble() * 0.1);
 
-        return coche.getVelocidadMaxima() * habilidad * factorAleatorio;
+        return coche.getVelocidadMaxima() * habilidad * rand.nextDouble(0.9,1);
     }
 
     public String getNombre(){return null;}
