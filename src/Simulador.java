ae18be97bb0d65178186b778ce186119f5dbe80b
Mikelangelot <miangelcabo@gmail.com>
2026-03-02 10:32:53 +0100

    Haciendo main clase simulador

--- src/Simulador.java
+++ src/Simulador.java
@@ -8,18 +8,20 @@ public class Simulador {
         Coche coche5 = new Coche("Mercedes", 360, 0.7);
         Coche coche6 = new Coche("Mercedes", 355, 0.6);
         //Creamos los 6 pilotos
-        Piloto piloto1 = new Piloto("Fernando",44,coche1,0.9);
-        Piloto piloto2 = new Piloto("Carlos",44,coche1,0.6);
-        Piloto piloto3 = new Piloto("Max",44,coche1,0.7);
-        Piloto piloto4 = new Piloto("Colapinto",44,coche1,0.6);
-        Piloto piloto5 = new Piloto("Hamilton",44,coche1,0.8);
-        Piloto piloto6 = new Piloto("Vettel",44,coche1,0.7);
+        Piloto piloto1 = new Piloto("Fernando",44,coche1,"Excelente");
+        Piloto piloto2 = new Piloto("Carlos",44,coche1,"Bueno");
+        Piloto piloto3 = new Piloto("Max",44,coche1,"Mediocre");
+        Piloto piloto4 = new Piloto("Colapinto",44,coche1,"Malo");
+        Piloto piloto5 = new Piloto("Hamilton",44,coche1,"Bueno");
+        Piloto piloto6 = new Piloto("Vettel",44,coche1,"Mediocre");
 
 
         Competicion competicion1 = new Competicion("GP de España",1);
         Competicion competicion2 = new Competicion("Gp de Bahrein", 2);
 
-        Carrera carrera1 = new Carrera(1,46,1,6);
+        Circuito circuito1 =  new Circuito("Barcelona", )
+
+        Carrera carrera1 = new Carrera(1,50,6,6);
         Carrera carrera2 = new Carrera(2,48,1,6);
         Carrera carrera3 = new Carrera(3,55,1,6);
 
