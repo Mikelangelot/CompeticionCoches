986de265a8db5ac3fe17146d102fc08e9b8ef544
Mikelangelot <miangelcabo@gmail.com>
2026-03-02 10:19:57 +0100

    empezando clase main simulador

--- src/Simulador.java
+++ src/Simulador.java
@@ -1,6 +1,28 @@
 public class Simulador {
     public static void main(String[] args) {
-        Coche coche1 = new Coche("alfa", 22, 0.9);
-        Competicion competicion1 = new Competicion("pepe", 11,12, 1);
+        //Creamos los 6 coches
+        Coche coche1 = new Coche("Aston Martin", 340, 0.4);
+        Coche coche2 = new Coche("Aston Martin", 337, 0.5);
+        Coche coche3 = new Coche("Red Bull", 380, 0.9);
+        Coche coche4 = new Coche("Red Bull", 375, 0.8);
+        Coche coche5 = new Coche("Mercedes", 360, 0.7);
+        Coche coche6 = new Coche("Mercedes", 355, 0.6);
+        //Creamos los 6 pilotos
+        Piloto piloto1 = new Piloto("Fernando",44,coche1,0.9);
+        Piloto piloto2 = new Piloto("Carlos",44,coche1,0.6);
+        Piloto piloto3 = new Piloto("Max",44,coche1,0.7);
+        Piloto piloto4 = new Piloto("Colapinto",44,coche1,0.6);
+        Piloto piloto5 = new Piloto("Hamilton",44,coche1,0.8);
+        Piloto piloto6 = new Piloto("Vettel",44,coche1,0.7);
+
+
+        Competicion competicion1 = new Competicion("GP de España",1);
+        Competicion competicion2 = new Competicion("Gp de Bahrein", 2);
+
+        Carrera carrera1 = new Carrera(1,46,1,6);
+        Carrera carrera2 = new Carrera(2,48,1,6);
+        Carrera carrera3 = new Carrera(3,55,1,6);
+
+
     }
 }
